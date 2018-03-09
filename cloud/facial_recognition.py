d5b21c1deb0bf390ed28c13626798c1c69dd10ee
Daniel Chau <daniel_chau@live.com>
2018-03-08 16:47:00 -0800

    url_for wrong

--- cloud/facial_recognition.py
+++ cloud/facial_recognition.py
@@ -37,7 +37,7 @@ def upload():
 		if file and allowed_file(file.filename):
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			return redirect(url_for('uploaded_file', filename=filename))
+			return redirect(url_for('upload', filename=filename))
 	return '''
 	<!doctype html>
 	<title>Upload new File</title>
