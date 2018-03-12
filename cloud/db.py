c85531680a5976a520236d395b5ec9a03b0ed819
Daniel Chau <daniel_chau@live.com>
2018-03-11 21:23:03 -0700

    add user to database

--- cloud/db.py
+++ cloud/db.py
@@ -6,24 +6,24 @@ db = SqliteExtDatabase('database.db')
 
 
 class BaseModel(Model):
-    class Meta:
-        database = db
+	class Meta:
+		database = db
 
 
 class Users(BaseModel):
-    username = CharField(unique=True)
-    password = CharField()
-    photo_encoding = BlobField()
+	username = CharField(unique=True)
+	password = CharField()
+	photo_encoding = BlobField()
 
 
 def addUser(user_id, photo_encoding):
-    t = Users.create(user_id=user_id, photo_encoding=photo_encoding)
-    t.save()
+	t = Users.create(user_id=user_id, photo_encoding=photo_encoding)
+	t.save()
 
 
 def getUser(user_id):
-    return Users.select().where(Transcript.user_id == user_id)
+	return Users.select().where(Transcript.user_id == user_id)
 
 def create_tables():
-    with db:
-        db.create_tables([Users])
+	with db:
+		db.create_tables([Users])
