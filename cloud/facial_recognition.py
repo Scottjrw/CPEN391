f44497b362be161d90fba90d54a13f1cee9bebdf
Daniel Chau <daniel_chau@live.com>
2018-03-13 14:08:47 -0700

    creating a session

--- cloud/facial_recognition.py
+++ cloud/facial_recognition.py
@@ -4,6 +4,7 @@ import json
 import os
 from flask import Flask, jsonify, make_response, abort, request, redirect, url_for
 from flask_uploads import UploadSet, configure_uploads, IMAGES
+from flask.ext.session import Session
 from flask import session
 from werkzeug import secure_filename
 from os.path import expanduser
