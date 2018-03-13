f07c6f075a78af055869b80d92f4a068d7419025
Jack Guo <jackguo380@gmail.com>
2018-03-13 14:46:26 -0700

    Begin HPS stuff

---
+++ testhps/testhps_bb.v
@@ -0,0 +1,140 @@
+
+module testhps (
+	clk_clk,
+	hps_mem_mem_a,
+	hps_mem_mem_ba,
+	hps_mem_mem_ck,
+	hps_mem_mem_ck_n,
+	hps_mem_mem_cke,
+	hps_mem_mem_cs_n,
+	hps_mem_mem_ras_n,
+	hps_mem_mem_cas_n,
+	hps_mem_mem_we_n,
+	hps_mem_mem_reset_n,
+	hps_mem_mem_dq,
+	hps_mem_mem_dqs,
+	hps_mem_mem_dqs_n,
+	hps_mem_mem_odt,
+	hps_mem_mem_dm,
+	hps_mem_oct_rzqin,
+	reset_reset_n,
+	vga_CLK,
+	vga_HS,
+	vga_VS,
+	vga_BLANK,
+	vga_SYNC,
+	vga_R,
+	vga_G,
+	vga_B,
+	hps_io_hps_io_emac1_inst_TX_CLK,
+	hps_io_hps_io_emac1_inst_TXD0,
+	hps_io_hps_io_emac1_inst_TXD1,
+	hps_io_hps_io_emac1_inst_TXD2,
+	hps_io_hps_io_emac1_inst_TXD3,
+	hps_io_hps_io_emac1_inst_RXD0,
+	hps_io_hps_io_emac1_inst_MDIO,
+	hps_io_hps_io_emac1_inst_MDC,
+	hps_io_hps_io_emac1_inst_RX_CTL,
+	hps_io_hps_io_emac1_inst_TX_CTL,
+	hps_io_hps_io_emac1_inst_RX_CLK,
+	hps_io_hps_io_emac1_inst_RXD1,
+	hps_io_hps_io_emac1_inst_RXD2,
+	hps_io_hps_io_emac1_inst_RXD3,
+	hps_io_hps_io_qspi_inst_IO0,
+	hps_io_hps_io_qspi_inst_IO1,
+	hps_io_hps_io_qspi_inst_IO2,
+	hps_io_hps_io_qspi_inst_IO3,
+	hps_io_hps_io_qspi_inst_SS0,
+	hps_io_hps_io_qspi_inst_CLK,
+	hps_io_hps_io_sdio_inst_CMD,
+	hps_io_hps_io_sdio_inst_D0,
+	hps_io_hps_io_sdio_inst_D1,
+	hps_io_hps_io_sdio_inst_CLK,
+	hps_io_hps_io_sdio_inst_D2,
+	hps_io_hps_io_sdio_inst_D3,
+	hps_io_hps_io_usb1_inst_D0,
+	hps_io_hps_io_usb1_inst_D1,
+	hps_io_hps_io_usb1_inst_D2,
+	hps_io_hps_io_usb1_inst_D3,
+	hps_io_hps_io_usb1_inst_D4,
+	hps_io_hps_io_usb1_inst_D5,
+	hps_io_hps_io_usb1_inst_D6,
+	hps_io_hps_io_usb1_inst_D7,
+	hps_io_hps_io_usb1_inst_CLK,
+	hps_io_hps_io_usb1_inst_STP,
+	hps_io_hps_io_usb1_inst_DIR,
+	hps_io_hps_io_usb1_inst_NXT,
+	hps_io_hps_io_uart0_inst_RX,
+	hps_io_hps_io_uart0_inst_TX,
+	hps_io_hps_io_i2c0_inst_SDA,
+	hps_io_hps_io_i2c0_inst_SCL);	
+
+	input		clk_clk;
+	output	[14:0]	hps_mem_mem_a;
+	output	[2:0]	hps_mem_mem_ba;
+	output		hps_mem_mem_ck;
+	output		hps_mem_mem_ck_n;
+	output		hps_mem_mem_cke;
+	output		hps_mem_mem_cs_n;
+	output		hps_mem_mem_ras_n;
+	output		hps_mem_mem_cas_n;
+	output		hps_mem_mem_we_n;
+	output		hps_mem_mem_reset_n;
+	inout	[31:0]	hps_mem_mem_dq;
+	inout	[3:0]	hps_mem_mem_dqs;
+	inout	[3:0]	hps_mem_mem_dqs_n;
+	output		hps_mem_mem_odt;
+	output	[3:0]	hps_mem_mem_dm;
+	input		hps_mem_oct_rzqin;
+	input		reset_reset_n;
+	output		vga_CLK;
+	output		vga_HS;
+	output		vga_VS;
+	output		vga_BLANK;
+	output		vga_SYNC;
+	output	[7:0]	vga_R;
+	output	[7:0]	vga_G;
+	output	[7:0]	vga_B;
+	output		hps_io_hps_io_emac1_inst_TX_CLK;
+	output		hps_io_hps_io_emac1_inst_TXD0;
+	output		hps_io_hps_io_emac1_inst_TXD1;
+	output		hps_io_hps_io_emac1_inst_TXD2;
+	output		hps_io_hps_io_emac1_inst_TXD3;
+	input		hps_io_hps_io_emac1_inst_RXD0;
+	inout		hps_io_hps_io_emac1_inst_MDIO;
+	output		hps_io_hps_io_emac1_inst_MDC;
+	input		hps_io_hps_io_emac1_inst_RX_CTL;
+	output		hps_io_hps_io_emac1_inst_TX_CTL;
+	input		hps_io_hps_io_emac1_inst_RX_CLK;
+	input		hps_io_hps_io_emac1_inst_RXD1;
+	input		hps_io_hps_io_emac1_inst_RXD2;
+	input		hps_io_hps_io_emac1_inst_RXD3;
+	inout		hps_io_hps_io_qspi_inst_IO0;
+	inout		hps_io_hps_io_qspi_inst_IO1;
+	inout		hps_io_hps_io_qspi_inst_IO2;
+	inout		hps_io_hps_io_qspi_inst_IO3;
+	output		hps_io_hps_io_qspi_inst_SS0;
+	output		hps_io_hps_io_qspi_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_io_hps_io_sdio_inst_D0;
+	inout		hps_io_hps_io_sdio_inst_D1;
+	output		hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_D2;
+	inout		hps_io_hps_io_sdio_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D0;
+	inout		hps_io_hps_io_usb1_inst_D1;
+	inout		hps_io_hps_io_usb1_inst_D2;
+	inout		hps_io_hps_io_usb1_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D4;
+	inout		hps_io_hps_io_usb1_inst_D5;
+	inout		hps_io_hps_io_usb1_inst_D6;
+	inout		hps_io_hps_io_usb1_inst_D7;
+	input		hps_io_hps_io_usb1_inst_CLK;
+	output		hps_io_hps_io_usb1_inst_STP;
+	input		hps_io_hps_io_usb1_inst_DIR;
+	input		hps_io_hps_io_usb1_inst_NXT;
+	input		hps_io_hps_io_uart0_inst_RX;
+	output		hps_io_hps_io_uart0_inst_TX;
+	inout		hps_io_hps_io_i2c0_inst_SDA;
+	inout		hps_io_hps_io_i2c0_inst_SCL;
+endmodule
