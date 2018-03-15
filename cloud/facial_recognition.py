8ffd8b7266b3c73402a6a72369bdbfa6cc2a18af
Daniel Chau <daniel_chau@live.com>
2018-03-15 16:25:09 -0700

    create new table for ifttt requests

--- cloud/facial_recognition.py
+++ cloud/facial_recognition.py
@@ -162,7 +162,7 @@ def addApplet():
 
 		cursor = db.cursor()
 
-		cursor.execute('''INSERT INTO applets VALUES (%s, %s)''', (user_id,request.form['applet']))
+		cursor.execute('''INSERT INTO applets VALUES (?, ?)''', (user_id,request.form['applet']))
 		requests = str(cursor.fetchone()[0])
 
 		return 'Added applet'
