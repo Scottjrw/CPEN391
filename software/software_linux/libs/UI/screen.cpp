fffdb8f0b90598c61ef04c0cbf9b85e98b71622e
Jack Guo <jackguo380@gmail.com>
2018-03-25 15:13:55 -0700

    refactor screen

--- software/software_linux/libs/UI/screen.cpp
+++ software/software_linux/libs/UI/screen.cpp
@@ -9,54 +9,58 @@
 #include <stdio.h>
 #include <chrono>
 
-using namespace UI;
+namespace UI {
 
-Screen::Screen(SimpleGraphics &graphics, TouchControl &touch):
+Screen::Screen(SimpleGraphics &graphics, TouchControl &touch,
+        unsigned width, unsigned height):
 	Drawable(graphics),
 	Touchable(touch),
+    m_width(width),
+    m_height(height),
 	m_exited(false),
 	m_drawables(),
 	m_touchables(),
 	m_next_screen(),
-	cursor(m_graphics, SimpleGraphics::rgba(0, 255, 0, 255), 4),
-    red_dot_cursor(m_graphics, SimpleGraphics::rgba(0, 0, 255, 255), 4)
+	m_cursor(m_graphics, SimpleGraphics::rgba(0, 255, 0, 255), 4),
+    m_red_dot_cursor(m_graphics, SimpleGraphics::rgba(0, 0, 255, 255), 4)
 {
 }
 
 void Screen::draw() {
-	Rectangle clear_rect(m_graphics, {0, 0}, {160, 120}, SimpleGraphics::rgba(0, 0, 0, 0));
-	clear_rect.draw();
-
 	for (unsigned i = 0; i < m_drawables.size(); i++) {
 		m_drawables[i]->draw();
 	}
 }
 
-void Screen::enable_touch() {
-
-	std::cout << "Enable Touch" << std::endl;
-	m_touch.setTouchCB([this](TouchControl *, unsigned x, unsigned y) {
-		Point p = {x, y};
-
-		// undraw previous cursor
-		// restore the image that previous cursor covers
-
-		// draw new cursor at Point p
-
-		cursor.undraw();
-
-		for (auto touch : m_touchables) {
-			if (touch->touch(p)) break;
-		}
-		cursor.update(p);
-        cursor.draw();
-	});
-}
-
-void Screen::clear() {
+void Screen::undraw() {
 	for (unsigned i = 0; i < m_drawables.size(); i++) {
 		m_drawables[i]->undraw();
 	}
+}
+
+void Screen::enable_touch() {
+    using namespace std::placeholders;
+
+	m_touch.setTouchCB(std::bind(&Screen::touchCB, this, _1, _2, _3));
+}
+
+void Screen::touchCB(TouchControl *screenptr, unsigned x, unsigned y) {
+
+    Point p = {x, y};
+
+    // undraw previous cursor
+    // restore the image that previous cursor covers
+
+    // draw new cursor at Point p
+
+    m_cursor.undraw();
+
+    for (auto touch : m_touchables) {
+        if (touch->touch(p)) break;
+    }
+
+    m_cursor.update(p);
+    m_cursor.draw();
 }
 
 
@@ -73,37 +77,39 @@ void Screen::addTouchable(Touchable* element) {
 // }
 
 
-Current_Screen Screen::run(void) {
-    std::chrono::time_point<std::chrono::high_resolution_clock> last_poll_ticks;
+int Screen::run(void) {
+    using namespace std::chrono;
+
+    auto last_poll_ticks = high_resolution_clock::now();
 	while(m_exited == false){
 		m_touch.trypoll();
 
-        std::chrono::time_point<std::chrono::high_resolution_clock> now = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> ms = (1000 * (now - last_poll_ticks)) ;   
+        auto now = high_resolution_clock::now();
+        milliseconds ms = duration_cast<milliseconds>(now - last_poll_ticks);
 
-        if (ms.count() > RED_DOT_POLL_MS) {
-            unsigned x, y, count;
-            //m_cluster->poll(x, y, count);
+        //if (ms.count() > RED_DOT_POLL_MS) {
+        //    unsigned x, y, count;
+        //    m_cluster->poll(x, y, count);
 
-            red_dot_cursor.undraw();
+        //    m_red_dot_cursor.undraw();
 
-            red_dot_cursor.update({x/2, y/2});
+        //    m_red_dot_cursor.update({x, y});
 
-            red_dot_cursor.draw();
+        //    m_red_dot_cursor.draw();
 
-            last_poll_ticks = now;
-        }
+        //    last_poll_ticks = now;
+        //}
 
 	}
+
 	return m_next_screen;
-
 }
 
-void Screen::exit(Current_Screen next_screen) {
+void Screen::exit(int ret_code) {
 	m_exited = true;
-	m_next_screen = next_screen;
+	m_next_screen = ret_code;
 }
 
-
+};
 
 
