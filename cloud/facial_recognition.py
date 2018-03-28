276f18aad82324a985d438c6269f6be981f70f31
Daniel Chau <daniel_chau@live.com>
2018-03-28 16:12:59 -0700

    fixed insertion into table

--- cloud/facial_recognition.py
+++ cloud/facial_recognition.py
@@ -72,11 +72,11 @@ def pictureSegement():
 		cursor = db.cursor()
 
 		if (cursor.lastrowid != 18):
-			cursor.execute('''INSERT INTO tempPicture VALUES (?)''', (hex_string))
+			tempPicture.create(picture_segment = hex_string)
 			return 'string added'
 		else:
 			cursor.execute('''TRUNCATE TABLE tempPicture''')
-			cursor.execute('''INSERT INTO tempPicture VALUES (?)''', (hex_string))
+			tempPicture.create(picture_segment = hex_string)
 			return 'string added and new table'
 
 	return 'string failed'
