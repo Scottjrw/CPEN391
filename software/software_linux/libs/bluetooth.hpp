8f4656f0a5b4e7549a36323be08ae7ec743a33f2
Daniel Chau <daniel_chau@live.com>
2018-04-03 12:40:21 -0700

    adding updated bluetooth libraries

--- software/software_linux/libs/bluetooth.hpp
+++ software/software_linux/libs/bluetooth.hpp
@@ -4,21 +4,72 @@
 #include <stdio.h>
 #include <string>
 #include <termios.h>
+#include <functional>
+#include <vector>
+#include "TermiosUtil.hpp"
 
-class Bluetooth {
+
+class Wand {
 public:
-    Bluetooth(const char name[]);
+    
+    enum Modes {
+        gestureMode,
+        typingMode,
+        cursorMode
+    };
+    
+    enum WandCommands {
+        wandStart,
+        wandStop
+    };
+    
+    Wand(const char name[], Baudrate rate);
+    
+    typedef std::function<void(Modes m)> ModeCB;
+    typedef std::function<void(WandCommands wc)> WandCommandCB;
+    
+    // Non-blocking receive, only possible with the unix-like interface
+    void trypoll();
+    
+    
+    void setValidModes(std::vector<Modes> modes);
     
     void LedOn();
     void LedOff();
-    std::string ReadResponse();
+    void setRemoteAddress(std::string address);
+    bool connectToAddress(std::string address);
+    void killConnection();
+    void setMode(std::string mode);
+    std::vector<std::vector<std::string>> getAvailableDevices(std::string time_length);
+    
+    void setMode(ModeCB cb){ m_modeCB = cb; };
+    void setGesture(WandCommandCB cb){ m_wandCommandCB = cb; };
+    
     int GetFd();
-    int openFile();
     void closeFile();
+    
+    bool checkConnection();
     
 private:
     int bluetooth_uart;
-    FILE * F;
+    unsigned m_recv_index;
+    ModeCB m_modeCB;
+    Modes m_current_mode;
+    std::vector<Modes> m_validModes;
+    bool m_connected;
+    
+    WandCommandCB m_wandCommandCB;
+    WandCommands m_current_wand;
+    
+    // Number of bytes to read at once for trypoll
+    static constexpr unsigned RECV_BYTES = 16;
+    
+    void recv(char val);
+    bool waitForResponse(std::string response);
+    std::vector<std::vector<std::string>> waitForInquiry(std::string time_length);
+    
+    void enterCommandMode();
+    void exitCommandMode();
     
 };
 
